e6d00d483987e04128ecca33ed678b05df4eac89
2222-42 <stephen0617@me.com>
2021-04-23 23:20:27 +0900

    feat(*): add quickSort

---
+++ src/quickSort.ts
@@ -0,0 +1,52 @@
+
+function quickSortSub(numbers: number[], startID, endID: number) {
+    let pivot = numbers[(startID + endID) / 2 | 0];
+    let left = startID;
+    let right = endID;
+
+    while (true) {
+        while (numbers[left] < pivot) {
+            left++;
+        }
+
+        while (pivot < numbers[right]) {
+            right--;
+        }
+
+        if (right <= left) {
+            break;
+        }
+
+        let tmp = numbers[left];
+        numbers[left] = numbers[right];
+        numbers[right] = tmp;
+        left++;
+        right--;
+    }
+
+    if (startID < left - 1) {
+        quickSortSub(numbers, startID, left - 1);
+    }
+
+    if (right + 1 < endID) {
+        quickSortSub(numbers, right + 1, endID);
+    }
+}
+
+function quickSort(numbers: number[]): number[] {
+    quickSortSub(numbers, 0, numbers.length - 1);
+    return numbers
+}
+
+let a = [10, 3, 1, 4, 2];
+let b = [10, 3, 1, 9, 6, 8, 2, 4, 5];
+let c = [10, 3, 1, 9, 7, 6, 8, 2, 4, 5];
+let resultA = quickSort(a);
+console.log(resultA);
+let resultB = quickSort(b);
+console.log(resultB);
+let resultC = quickSort(c);
+console.log(resultC);
+
+
+export { quickSort }
